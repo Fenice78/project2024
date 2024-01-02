d0c09702fdf12a35ae485a6ff61911053ab2be31
Fenice78 <127755873+Fenice78@users.noreply.github.com>
2024-01-03 00:16:23 +0100

    Delete WordAnimation/script.js

--- WordAnimation/script.js
+++
@@ -1,7 +0,0 @@
-new Vue({
-    el:'#franzFerdinand',
-    data:{
-      franz:['ff6b08','FF781E','FF8534','FF934B','FFA061','FFAE78','FFBB8E','FFC9A5','FFD6BB','FFE4D2','FFF1E8'],
-      ferdinand:['EF0891','F01E9B','F134A5','F34BAF','F461B9','F678C3','F78ECD','F9A5D7','FABBE1','FCD2EB','FDE8F5']
-    }
-  });
