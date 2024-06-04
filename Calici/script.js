a92392bd6ce1b910b449c7e6610e04fa45cd938b
Creative Web B Beatrice Bottini <127755873+Fenice78@users.noreply.github.com>
2024-06-04 16:21:48 +0200

    Update script.js

--- Calici/script.js
+++ Calici/script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
         for (let i = 0; i < numBubbles; i++) {
             const bubble = document.createElement('div');
             bubble.className = 'bubble';
-            bubble.style.left = `${Math.random() * (glass.offsetWidth - 10)}px`; // Posizionamento casuale all'interno del bicchiere
+            bubble.style.left = `${Math.random() * (glass.offsetWidth - 10)}px`; 
             bubble.style.animationDuration = `${Math.random() * 2 + 3}s`;
             bubble.style.animationDelay = `${Math.random() * 5}s`;
             champagne.appendChild(bubble);
